8007ff0f7b7616324db6fd11f72cd187a3e6a94f
linasobhy8-source <linasobhy8@gmail.com>
2026-03-22 18:55:45 +0200

    Update and rename index.html to index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-<div class="products"></div>
-
-<script>
 async function loadProducts() {
   const res = await fetch("/api/products");
   const data = await res.json();
 
   const container = document.querySelector(".products");
 
+  container.innerHTML = "";
+
   data.forEach(p => {
     container.innerHTML += `
       <div class="card">
+        <img src="${p.image}" />
+
         <h3>${p.title}</h3>
         <p>$${p.price}</p>
+
+        <button onclick='addToCart(${JSON.stringify(p)})'>
+          Add to Cart 🛒
+        </button>
 
         <a href="product.html?id=${p.id}">
           عرض التفاصيل
@@ -22,4 +27,3 @@ async function loadProducts() {
 }
 
 loadProducts();
-</script>
