1105629e142ce88b54600e7e4bde70a9605812b5
linasobhy8-source <linasobhy8@gmail.com>
2026-03-22 18:13:06 +0200

    Create products.js

---
+++ api/products.js
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+  res.status(200).json([
+    {
+      id: 1,
+      title: "Smart Watch",
+      price: 39.99,
+      image: "https://m.media-amazon.com/images/I/61IMRs+o0iL._AC_SL1500_.jpg",
+      link: "https://www.amazon.eg/dp/B09V7Z4TJG?tag=onlinesh03f31-21"
+    },
+    {
+      id: 2,
+      title: "Anker Speaker",
+      price: 49.99,
+      image: "https://m.media-amazon.com/images/I/71tV4O0rO0L._AC_SL1500_.jpg",
+      link: "https://www.amazon.eg/dp/B07ZNT7PRL?tag=onlinesh03f31-21"
+    }
+  ]);
+}
