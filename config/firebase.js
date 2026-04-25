09e490f68804a626b7bc77a65ac17737e3868fc0
linasobhy8-source <linasobhy8@gmail.com>
2026-04-25 20:10:17 +0300

    Update firebase.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,7 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+/* 🔥 Firebase Config */
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "onlinekoloonlineonline-a9979.firebaseapp.com",
@@ -10,6 +11,8 @@ const firebaseConfig = {
   appId: "1:1051818718615:web:9d3fd0d26d902082b3c0d2"
 };
 
-const app = initializeApp(firebaseConfig);
+/* 🔥 Prevent multiple initialization (IMPORTANT for Next.js) */
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
+/* 🔥 Firestore */
 export const db = getFirestore(app);
