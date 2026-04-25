5f43065b5562694134a62282e28967e7ec8e5822
linasobhy8-source <linasobhy8@gmail.com>
2026-04-25 19:51:09 +0300

    Update firebase-config.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+/* 🔥 Firebase Config */
 const firebaseConfig = {
-  apiKey: "XXX",
-  authDomain: "XXX",
-  projectId: "XXX",
-  appId: "XXX"
+  apiKey: "AIzaSyBlLFhmWdhqjf62VraY0yT9UgcaAn6jnhI",
+  authDomain: "onlinekoloonlineonline-a9979.firebaseapp.com",
+  projectId: "onlinekoloonlineonline-a9979",
+  storageBucket: "onlinekoloonlineonline-a9979.firebasestorage.app",
+  messagingSenderId: "1051818718615",
+  appId: "1:1051818718615:web:9d3fd0d26d902082b3c0d2"
 };
 
+/* 🔥 Init Firebase */
 const app = initializeApp(firebaseConfig);
 
+/* 🔥 Firestore */
 const db = getFirestore(app);
+
+export { db };
