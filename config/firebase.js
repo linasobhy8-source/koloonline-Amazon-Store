b351b5fc07a9ed0f3ae44f38fc9105db76f4b49a
linasobhy8-source <linasobhy8@gmail.com>
2026-04-25 13:14:07 +0300

    Update firebase.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- config/firebase.js
+++ config/firebase.js
@@ -2,16 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "koloonline.firebaseapp.com",
-  projectId: "koloonline",
-  storageBucket: "koloonline.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyBlLFhmWdhqjf62VraY0yT9UgcaAn6jnhI",
+  authDomain: "onlinekoloonlineonline-a9979.firebaseapp.com",
+  projectId: "onlinekoloonlineonline-a9979",
+  storageBucket: "onlinekoloonlineonline-a9979.firebasestorage.app",
+  messagingSenderId: "1051818718615",
+  appId: "1:1051818718615:web:9d3fd0d26d902082b3c0d2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore(app);
