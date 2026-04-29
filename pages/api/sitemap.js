2a5a17fa2cab09aaca9fb3064f649030b039f9d1
linasobhy8-source <linasobhy8@gmail.com>
2026-04-29 03:19:25 +0300

    Update sitemap.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -2,35 +2,54 @@ import { db } from "../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 
 export default async function handler(req, res) {
+  try {
+    const baseUrl = "https://koloonline.online";
 
-  const baseUrl = "https://koloonline.online";
+    const snap = await getDocs(collection(db, "products"));
+    const products = snap.docs.map((doc) => doc.data());
 
-  const snap = await getDocs(collection(db, "products"));
-
-  const products = snap.docs.map(doc => doc.data());
-
-  let urls = `
-    <url>
-      <loc>${baseUrl}</loc>
-      <priority>1.0</priority>
-    </url>
-  `;
-
-  products.forEach(p => {
-    urls += `
+    let urls = `
       <url>
-        <loc>${baseUrl}/product?asin=${p.asin}</loc>
-        <priority>0.9</priority>
+        <loc>${baseUrl}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <priority>1.0</priority>
+      </url>
+
+      <url>
+        <loc>${baseUrl}/blog</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <priority>0.8</priority>
+      </url>
+
+      <url>
+        <loc>${baseUrl}/contact</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <priority>0.7</priority>
       </url>
     `;
-  });
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls}
-  </urlset>`;
+    products.forEach((p) => {
+      if (!p.asin) return;
 
-  res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
+      urls += `
+        <url>
+          <loc>${baseUrl}/product/${p.asin}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <priority>0.9</priority>
+        </url>
+      `;
+    });
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+    res.setHeader("Content-Type", "application/xml");
+    res.status(200).send(sitemap);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error generating sitemap");
+  }
 }
