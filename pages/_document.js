327dbfac5d0efe7566f031a0f8d24be326737583
linasobhy8-source <linasobhy8@gmail.com>
2026-04-29 23:28:01 +0300

    Update _document.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_document.js
+++ pages/_document.js
@@ -2,20 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
-      <Head>
-
-        {/* ================= ADSENSE GLOBAL ================= */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        ></script>
-
-        {/* ================= SEO GLOBAL ================= */}
-        <meta name="google-site-verification" content="PUT-YOUR-CODE-HERE" />
-
-      </Head>
-
+    <Html lang="en">
+      <Head />
       <body>
         <Main />
         <NextScript />
