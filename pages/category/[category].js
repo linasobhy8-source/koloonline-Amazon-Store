8dd3f79c0a1cf00a9fc8384ed656f19d4137f98e
linasobhy8-source <linasobhy8@gmail.com>
2026-05-03 23:45:21 +0300

    Update [category].js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../firebase-config";
+import { db } from "../../config/firebase";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
@@ -15,22 +15,33 @@ export default function CategoryPage() {
   }, [category]);
 
   async function fetchProducts() {
-    const snap = await getDocs(collection(db, "products"));
+    try {
+      const snap = await getDocs(collection(db, "products"));
 
-    const filtered = snap.docs
-      .map((d) => ({ id: d.id, ...d.data() }))
-      .filter((p) => p.category === category);
+      const filtered = snap.docs
+        .map((d) => ({ id: d.id, ...d.data() }))
+        .filter((p) => p.category === category);
 
-    setProducts(filtered);
+      setProducts(filtered);
+    } catch (err) {
+      console.error(err);
+      setProducts([]);
+    }
   }
 
   return (
     <div style={{ padding: 20 }}>
       <h1>📦 {category}</h1>
 
-      <div style={{ display: "grid", gridTemplateColumns: "repeat(3,1fr)", gap: 20 }}>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3,1fr)",
+          gap: 20,
+        }}
+      >
         {products.map((p) => (
-          <Link key={p.id} href={`/products/${p.id}`}>
+          <Link key={p.id} href={`/product/${p.asin || p.id}`}>
             <div style={card}>
               <img src={p.image} width="100%" />
               <h4>{p.title}</h4>
