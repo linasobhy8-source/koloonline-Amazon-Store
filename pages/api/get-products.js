fb34918a35aef68d8c91fdbea5a15f07eee84efe
linasobhy8-source <linasobhy8@gmail.com>
2026-05-03 03:21:52 +0300

    Update get-products.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/api/get-products.js
+++ pages/api/get-products.js
@@ -1,13 +1,24 @@
-import { db } from "../../config/firebase";
-import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore, collection, getDocs, query, orderBy, limit } from "firebase/firestore";
 
+/* ================= FIREBASE INIT ================= */
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+const db = getFirestore(app);
+
+/* ================= HANDLER ================= */
 export default async function handler(req, res) {
   try {
     const { category, max = 50 } = req.query;
 
     let q;
 
-    /* ================= QUERY BUILD ================= */
+    /* ================= QUERY ================= */
     if (category) {
       q = query(
         collection(db, "products"),
