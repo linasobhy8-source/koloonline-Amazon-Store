dbaa71011bd9f48cd47d855a444718cc01bb38e8
linasobhy8-source <linasobhy8@gmail.com>
2026-05-06 23:52:50 +0300

    Create contact.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Contact() {
+  return (
+    <div style={{ padding: 20 }}>
+      <Head>
+        <title>Contact Us - Koloonline</title>
+      </Head>
+
+      <h1>Contact Us</h1>
+
+      <p>Email: support@koloonline.online</p>
+
+      <p>If you have any questions, feel free to contact us anytime.</p>
+    </div>
+  );
+}
