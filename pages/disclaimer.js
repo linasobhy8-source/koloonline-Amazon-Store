1c1dc66712d3b6c38ea93c231a320dc876864de7
linasobhy8-source <linasobhy8@gmail.com>
2026-05-07 00:00:56 +0300

    Create disclaimer.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

---
+++ pages/disclaimer.js
@@ -0,0 +1,12 @@
+export default function Disclaimer() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Affiliate Disclaimer</h1>
+
+      <p>
+        This site contains affiliate links. We may earn a commission when you
+        purchase through these links at no extra cost to you.
+      </p>
+    </div>
+  );
+}
