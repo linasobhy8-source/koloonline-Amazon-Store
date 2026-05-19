6b78f83c6f1e11953c34a4a778ff41f94a08942d
linasobhy8-source <linasobhy8@gmail.com>
2026-05-19 03:10:06 +0300

    Create keywordMiner.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

---
+++ engine/keywordMiner.js
@@ -0,0 +1,8 @@
+export function keywordMiner() {
+  return [
+    { keyword: "cheap gaming headset", score: 92 },
+    { keyword: "best air fryer 2026", score: 88 },
+    { keyword: "amazon home gadgets", score: 85 },
+    { keyword: "smart watch under 50", score: 83 },
+  ];
+}
