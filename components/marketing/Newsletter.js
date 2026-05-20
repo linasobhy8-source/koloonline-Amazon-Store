c4238dbab842e326378c614976b444057c96b9f4
linasobhy8-source <linasobhy8@gmail.com>
2026-05-20 18:13:20 +0300

    Add Newsletter component for daily deals subscription
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

---
+++ components/marketing/Newsletter.js
@@ -0,0 +1,24 @@
+export default function Newsletter() {
+  return (
+    <div
+      style={{
+        background: "#111827",
+        color: "white",
+        padding: 40,
+        borderRadius: 20,
+        textAlign: "center",
+      }}
+    >
+      <h2>Get Daily Amazon Deals</h2>
+
+      <input
+        placeholder="Enter your email"
+        style={{
+          padding: 14,
+          width: 300,
+          marginTop: 15,
+        }}
+      />
+    </div>
+  );
+}
