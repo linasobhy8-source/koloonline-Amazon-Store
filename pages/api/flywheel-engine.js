3add31d6e65a282bf1fb4bc3dc42df6dc975de31
linasobhy8-source <linasobhy8@gmail.com>
2026-05-30 17:18:07 +0300

    Delete pages/api/flywheel-engine.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/api/flywheel-engine.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    success: false,
-    disabled: true,
-    message: "flywheel-engine is disabled for performance",
-  });
-}
