f7b2928f6afbb60ddc6591e0f3eb5beeda68c1b3
linasobhy8-source <linasobhy8@gmail.com>
2026-05-30 03:53:04 +0300

    Delete pages/api/auto-create-page.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/api/auto-create-page.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    success: false,
-    disabled: true,
-    message: "auto-create-page disabled temporarily",
-  });
-}
