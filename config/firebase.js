d7e13d45c069a27061d73f9fb668b08dbc98ef21
linasobhy8-source <linasobhy8@gmail.com>
2026-05-31 05:59:24 +0300

    Update firebase.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- config/firebase.js
+++ config/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-/* ================= INIT SAFELY ================= */
+/* ================= SINGLETON ================= */
 const app = !getApps().length
   ? initializeApp(firebaseConfig)
   : getApps()[0];
