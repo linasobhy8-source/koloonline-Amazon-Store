cc67a38efd41fa556ef732b912b4537c5a9f7068
linasobhy8-source <linasobhy8@gmail.com>
2026-06-02 05:04:20 +0300

    Refactor traffic weight calculation in trafficController
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- os/trafficController.js
+++ os/trafficController.js
@@ -1,17 +1,22 @@
 export function trafficController(products = []) {
   return products
     .map((p) => {
-      let weight = 1;
+      let trafficWeight = 1;
 
-      if (p.viralBoost) weight += 3;
-      if (p.trendScore > 50) weight += 2;
-      if (p.views > 1000) weight += 1;
+      const trendScore = p.trendScore || 0;
+      const views = p.views || 0;
 
-      if (p.trendScore < 20) weight *= 0.5;
+      /* ================= POSITIVE SIGNALS ================= */
+      if (p.viralBoost) trafficWeight += 3;
+      if (trendScore > 50) trafficWeight += 2;
+      if (views > 1000) trafficWeight += 1;
+
+      /* ================= NEGATIVE SIGNALS ================= */
+      if (trendScore < 20) trafficWeight *= 0.5;
 
       return {
         ...p,
-        trafficWeight: weight,
+        trafficWeight,
       };
     })
     .sort((a, b) => b.trafficWeight - a.trafficWeight);
