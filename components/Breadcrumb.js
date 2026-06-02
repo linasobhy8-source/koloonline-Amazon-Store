36e644c332708cee4d2b9ebac29162fb02eea056
linasobhy8-source <linasobhy8@gmail.com>
2026-06-02 05:45:24 +0300

    Refactor Breadcrumb component to use Link from Next.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -1,22 +1,45 @@
+import Link from "next/link";
+import Head from "next/head";
+
 export default function Breadcrumb({ items = [] }) {
+  const baseUrl = "https://koloonline.online";
+
   return (
-    <div style={styles.container}>
+    <>
+      {/* ================= SEO STRUCTURED DATA (IMPORTANT FOR GOOGLE + ADSENSE) ================= */}
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "BreadcrumbList",
+              itemListElement: items.map((item, index) => ({
+                "@type": "ListItem",
+                position: index + 1,
+                name: item.name,
+                item: `${baseUrl}${item.link}`,
+              })),
+            }),
+          }}
+        />
+      </Head>
 
-      {items.map((item, index) => (
-        <span key={index} style={styles.item}>
-          
-          <a href={item.link} style={styles.link}>
-            {item.name}
-          </a>
+      {/* ================= BREADCRUMB UI ================= */}
+      <nav aria-label="breadcrumb" style={styles.container}>
+        {items.map((item, index) => (
+          <span key={index} style={styles.item}>
+            <Link href={item.link} style={styles.link}>
+              {item.name}
+            </Link>
 
-          {index < items.length - 1 && (
-            <span style={styles.separator}>›</span>
-          )}
-
-        </span>
-      ))}
-
-    </div>
+            {index < items.length - 1 && (
+              <span style={styles.separator}>›</span>
+            )}
+          </span>
+        ))}
+      </nav>
+    </>
   );
 }
 
@@ -40,6 +63,7 @@ const styles = {
   link: {
     color: "#007185",
     textDecoration: "none",
+    fontWeight: 500,
   },
 
   separator: {
