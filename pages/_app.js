efce80a5fe724fba21070dd7ff5e6f45f65de885
linasobhy8-source <linasobhy8@gmail.com>
2026-06-02 06:39:07 +0300

    Update _app.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Script from "next/script";
-
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Navbar from "../components/layout/Navbar";
@@ -27,19 +26,33 @@ export default function App({ Component, pageProps }) {
           content="index,follow,max-image-preview:large"
         />
 
-        <link rel="icon" href="/favicon.ico" />
-
-        {/* SEO SAFE DEFAULTS */}
         <meta name="googlebot" content="index,follow" />
+
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* ================= GOOGLE ANALYTICS ================= */}
+      {/* ================= GOOGLE TAG MANAGER (GTM) ================= */}
+      <Script
+        id="gtm-script"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KNQM8KBN');
+          `,
+        }}
+      />
+
+      {/* ================= GOOGLE ANALYTICS 4 ================= */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-YS8L61XLPR"
         strategy="afterInteractive"
       />
 
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="ga4-script" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
@@ -51,6 +64,16 @@ export default function App({ Component, pageProps }) {
           });
         `}
       </Script>
+
+      {/* ================= GTM NOSCRIPT (fallback) ================= */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-KNQM8KBN"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
 
       {/* ================= LAYOUT ================= */}
       <Navbar />
