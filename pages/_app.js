0c7473c0e0031319fd3a7e62a88a31d4de5217f9
linasobhy8-source <linasobhy8@gmail.com>
2026-06-03 18:17:33 +0300

    Add product preloading and refactor Head component
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,172 +1,172 @@
 import Head from "next/head";
 import Script from "next/script";
+import { useEffect } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
+import { getProductsFast } from "../lib/firebaseQuery";
 
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-return (
-<>
-<Head>
-<meta charSet="UTF-8" />
+  /* ================= PRELOAD PRODUCTS (TURBO BOOST) ================= */
+  useEffect(() => {
+    getProductsFast(); // 🔥 preload in background (no blocking)
+  }, []);
 
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1"
-    />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
 
-    <meta
-      name="robots"
-      content="index,follow,max-image-preview:large"
-    />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
 
-    <meta
-      name="googlebot"
-      content="index,follow,max-image-preview:large"
-    />
+        <meta
+          name="robots"
+          content="index,follow,max-image-preview:large"
+        />
 
-    <meta name="theme-color" content="#111827" />
+        <meta
+          name="googlebot"
+          content="index,follow,max-image-preview:large"
+        />
 
-    <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#111827" />
 
-    <link
-      rel="preconnect"
-      href="https://pagead2.googlesyndication.com"
-    />
+        <link rel="icon" href="/favicon.ico" />
 
-    <link
-      rel="preconnect"
-      href="https://www.googletagmanager.com"
-    />
+        <link
+          rel="preconnect"
+          href="https://pagead2.googlesyndication.com"
+        />
 
-    <link
-      rel="dns-prefetch"
-      href="https://www.googletagmanager.com"
-    />
+        <link
+          rel="preconnect"
+          href="https://www.googletagmanager.com"
+        />
 
-    <link
-      rel="dns-prefetch"
-      href="https://pagead2.googlesyndication.com"
-    />
-  </Head>
+        <link
+          rel="dns-prefetch"
+          href="https://www.googletagmanager.com"
+        />
 
-  {/* ================= GTM ================= */}
-  <Script
-    id="gtm-script"
-    strategy="afterInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-      (function(w,d,s,l,i){
-        w[l]=w[l]||[];
-        w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});
-        var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),
-        dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true;
-        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-        f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-KNQM8KBN');
-      `,
-    }}
-  />
+        <link
+          rel="dns-prefetch"
+          href="https://pagead2.googlesyndication.com"
+        />
+      </Head>
 
-  {/* ================= GA4 ================= */}
-  <Script
-    src="https://www.googletagmanager.com/gtag/js?id=G-YS8L61XLPR"
-    strategy="lazyOnload"
-  />
+      {/* ================= GTM ================= */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KNQM8KBN');
+          `,
+        }}
+      />
 
-  <Script
-    id="ga4-script"
-    strategy="lazyOnload"
-  >
-    {`
-      window.dataLayer = window.dataLayer || [];
+      {/* ================= GA4 ================= */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-YS8L61XLPR"
+        strategy="lazyOnload"
+      />
 
-      function gtag(){
-        dataLayer.push(arguments);
-      }
+      <Script id="ga4-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
 
-      gtag('js', new Date());
+          function gtag(){
+            dataLayer.push(arguments);
+          }
 
-      gtag('config', 'G-YS8L61XLPR', {
-        page_path: window.location.pathname,
-        anonymize_ip: true
-      });
-    `}
-  </Script>
+          gtag('js', new Date());
 
-  {/* ================= FACEBOOK PIXEL ================= */}
-  <Script
-    id="facebook-pixel"
-    strategy="lazyOnload"
-  >
-    {`
-      !function(f,b,e,v,n,t,s)
-      {
-        if(f.fbq)return;
-        n=f.fbq=function(){
-          n.callMethod ?
-          n.callMethod.apply(n,arguments) :
-          n.queue.push(arguments)
-        };
-        if(!f._fbq)f._fbq=n;
-        n.push=n;
-        n.loaded=!0;
-        n.version='2.0';
-        n.queue=[];
-        t=b.createElement(e);
-        t.async=!0;
-        t.src=v;
-        s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)
-      }(
-        window,
-        document,
-        'script',
-        'https://connect.facebook.net/en_US/fbevents.js'
-      );
+          gtag('config', 'G-YS8L61XLPR', {
+            page_path: window.location.pathname,
+            anonymize_ip: true
+          });
+        `}
+      </Script>
 
-      fbq('init', '1038760229314882');
-      fbq('track', 'PageView');
-    `}
-  </Script>
+      {/* ================= FACEBOOK PIXEL ================= */}
+      <Script id="facebook-pixel" strategy="lazyOnload">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {
+            if(f.fbq)return;
+            n=f.fbq=function(){
+              n.callMethod ?
+              n.callMethod.apply(n,arguments) :
+              n.queue.push(arguments)
+            };
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)
+          }(
+            window,
+            document,
+            'script',
+            'https://connect.facebook.net/en_US/fbevents.js'
+          );
 
-  {/* ================= ADSENSE ================= */}
-  <Script
-    async
-    strategy="lazyOnload"
-    crossOrigin="anonymous"
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1294940976431468"
-  />
+          fbq('init', '1038760229314882');
+          fbq('track', 'PageView');
+        `}
+      </Script>
 
-  {/* ================= GTM NOSCRIPT ================= */}
-  <noscript>
-    <iframe
-      src="https://www.googletagmanager.com/ns.html?id=GTM-KNQM8KBN"
-      height="0"
-      width="0"
-      style={{
-        display: "none",
-        visibility: "hidden",
-      }}
-    />
-  </noscript>
+      {/* ================= ADSENSE ================= */}
+      <Script
+        async
+        strategy="lazyOnload"
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1294940976431468"
+      />
 
-  <Navbar />
+      {/* ================= GTM NOSCRIPT ================= */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-KNQM8KBN"
+          height="0"
+          width="0"
+          style={{
+            display: "none",
+            visibility: "hidden",
+          }}
+        />
+      </noscript>
 
-  <main>
-    <Component {...pageProps} />
-  </main>
+      <Navbar />
 
-  <Footer />
+      <main>
+        <Component {...pageProps} />
+      </main>
 
-  <SpeedInsights />
-</>
+      <Footer />
 
-);
-        }
+      <SpeedInsights />
+    </>
+  );
+              }
