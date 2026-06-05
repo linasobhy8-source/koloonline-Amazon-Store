0b34e3c522d7042e693a4611bf6a9648f476e51d
linasobhy8-source <linasobhy8@gmail.com>
2026-06-05 03:05:59 +0300

    Update _app.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -7,142 +7,208 @@ import Footer from "../components/layout/Footer";
 
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-return (
-<>
-<Head>
-<meta charSet="UTF-8" />
+export default function App({
+  Component,
+  pageProps,
+}) {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
 
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1"
-    />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
 
-    <meta
-      name="robots"
-      content="index,follow,max-image-preview:large"
-    />
+        <meta
+          name="robots"
+          content="index,follow,max-image-preview:large"
+        />
 
-    <meta
-      name="googlebot"
-      content="index,follow,max-image-preview:large"
-    />
+        <meta
+          name="googlebot"
+          content="index,follow,max-image-preview:large"
+        />
 
-    <meta name="theme-color" content="#111827" />
+        <meta
+          name="theme-color"
+          content="#111827"
+        />
 
-    <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
 
-    <link
-      rel="preconnect"
-      href="https://www.googletagmanager.com"
-    />
+        {/* PERFORMANCE */}
+        <link
+          rel="preconnect"
+          href="https://www.googletagmanager.com"
+          crossOrigin="anonymous"
+        />
 
-    <link
-      rel="dns-prefetch"
-      href="https://www.googletagmanager.com"
-    />
+        <link
+          rel="preconnect"
+          href="https://connect.facebook.net"
+          crossOrigin="anonymous"
+        />
 
-    <link
-      rel="preconnect"
-      href="https://pagead2.googlesyndication.com"
-    />
+        <link
+          rel="dns-prefetch"
+          href="https://www.googletagmanager.com"
+        />
 
-    <link
-      rel="dns-prefetch"
-      href="https://pagead2.googlesyndication.com"
-    />
-  </Head>
+        <link
+          rel="dns-prefetch"
+          href="https://connect.facebook.net"
+        />
+      </Head>
 
-  <Script
-    id="gtm-script"
-    strategy="lazyOnload"
-    dangerouslySetInnerHTML={{
-      __html: `
-        (function(w,d,s,l,i){
-          w[l]=w[l]||[];
-          w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});
-          var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),
-          dl=l!='dataLayer'?'&l='+l:'';
-          j.async=true;
-          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KNQM8KBN');
-      `,
-    }}
-  />
+      {/* ================= GOOGLE TAG MANAGER ================= */}
 
-  <Script
-    src="https://www.googletagmanager.com/gtag/js?id=G-YS8L61XLPR"
-    strategy="lazyOnload"
-  />
+      <Script
+        id="gtm-script"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'
+                ? '&l='+l
+                : '';
+              j.async=true;
+              j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+
+                i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(
+              window,
+              document,
+              'script',
+              'dataLayer',
+              'GTM-KNQM8KBN'
+            );
+          `,
+        }}
+      />
 
-  <Script id="ga4-script" strategy="lazyOnload">
-    {`
-      window.dataLayer = window.dataLayer || [];
+      {/* ================= GA4 ================= */}
 
-      function gtag(){
-        dataLayer.push(arguments);
-      }
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-YS8L61XLPR"
+        strategy="lazyOnload"
+      />
 
-      gtag('js', new Date());
+      <Script
+        id="ga4-script"
+        strategy="lazyOnload"
+      >
+        {`
+          window.dataLayer =
+            window.dataLayer || [];
 
-      gtag('config', 'G-YS8L61XLPR', {
-        anonymize_ip: true
-      });
-    `}
-  </Script>
+          function gtag(){
+            dataLayer.push(arguments);
+          }
 
-  <Script id="facebook-pixel" strategy="lazyOnload">
-    {`
-      !function(f,b,e,v,n,t,s)
-      {
-        if(f.fbq)return;
-        n=f.fbq=function(){
-          n.callMethod ?
-          n.callMethod.apply(n,arguments) :
-          n.queue.push(arguments)
-        };
-        if(!f._fbq)f._fbq=n;
-        n.push=n;
-        n.loaded=!0;
-        n.version='2.0';
-        n.queue=[];
-        t=b.createElement(e);
-        t.async=!0;
-        t.src=v;
-        s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)
-      }(
-        window,
-        document,
-        'script',
-        'https://connect.facebook.net/en_US/fbevents.js'
-      );
+          gtag('js', new Date());
 
-      fbq('init', '1038760229314882');
-      fbq('track', 'PageView');
-    `}
-  </Script>
+          gtag(
+            'config',
+            'G-YS8L61XLPR',
+            {
+              anonymize_ip: true,
+              send_page_view: true
+            }
+          );
+        `}
+      </Script>
 
-  <Script
-    async
-    strategy="lazyOnload"
-    crossOrigin="anonymous"
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1294940976431468"
-  />
+      {/* ================= FACEBOOK PIXEL ================= */}
 
-  <Navbar />
+      <Script
+        id="facebook-pixel"
+        strategy="lazyOnload"
+      >
+        {`
+          !function(f,b,e,v,n,t,s){
+            if(f.fbq)return;
+            n=f.fbq=function(){
+              n.callMethod
+                ? n.callMethod.apply(
+                    n,
+                    arguments
+                  )
+                : n.queue.push(
+                    arguments
+                  );
+            };
 
-  <main>
-    <Component {...pageProps} />
-  </main>
+            if(!f._fbq)
+              f._fbq=n;
 
-  <Footer />
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
 
-  <SpeedInsights />
-</>
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
 
-);
-        }
+            s=b.getElementsByTagName(e)[0];
+
+            s.parentNode.insertBefore(
+              t,
+              s
+            );
+          }(
+            window,
+            document,
+            'script',
+            'https://connect.facebook.net/en_US/fbevents.js'
+          );
+
+          fbq(
+            'init',
+            '1038760229314882'
+          );
+
+          fbq(
+            'track',
+            'PageView'
+          );
+        `}
+      </Script>
+
+      {/* ================= ADSENSE ================= */}
+
+      <Script
+        id="adsense"
+        strategy="lazyOnload"
+        async
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1294940976431468"
+      />
+
+      {/* ================= PAGE ================= */}
+
+      <Navbar />
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+
+      <Footer />
+
+      <SpeedInsights />
+    </>
+  );
+              }
