be25682abf4c3441a02af0c4656468d3ccb7bf27
linasobhy8-source <linasobhy8@gmail.com>
2026-06-05 04:21:43 +0300

    Update robots meta tag and GTM script
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -13,15 +13,21 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="index,follow" />
+        <meta name="robots" content="index,follow,max-image-preview:large" />
         <meta name="theme-color" content="#111827" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* 🔥 Deferred Analytics (No blocking render) */}
+      {/* GTM (lazy فقط) */}
       <Script
+        id="gtm"
         strategy="lazyOnload"
-        src="https://www.googletagmanager.com/gtag/js?id=G-YS8L61XLPR"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
+          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KNQM8KBN');`,
+        }}
       />
 
       <Navbar />
@@ -35,4 +41,4 @@ export default function App({ Component, pageProps }) {
       <SpeedInsights />
     </>
   );
-    }
+        }
