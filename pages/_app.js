1c6924d90fd3632b7bd81685cf9880afe1df3811
linasobhy8-source <linasobhy8@gmail.com>
2026-06-05 04:10:33 +0300

    Update _app.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
-import "../styles/globals.css";
+import Head from "next/head";
+import Script from "next/script";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
+
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index,follow" />
+        <meta name="theme-color" content="#111827" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* 🔥 Deferred Analytics (No blocking render) */}
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-YS8L61XLPR"
+      />
+
       <Navbar />
 
       <main>
@@ -12,6 +31,8 @@ export default function App({ Component, pageProps }) {
       </main>
 
       <Footer />
+
+      <SpeedInsights />
     </>
   );
-}
+    }
