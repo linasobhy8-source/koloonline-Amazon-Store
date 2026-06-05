9d6d5165fbc4411bb9e7465cfb7d0f770a11a215
linasobhy8-source <linasobhy8@gmail.com>
2026-06-05 03:12:11 +0300

    Update _app.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,208 +1,127 @@
 import Head from "next/head";
 import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { useEffect } from "react";
 
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 
 import "../styles/globals.css";
 
-export default function App({
-  Component,
-  pageProps,
-}) {
+export default function App({ Component, pageProps }) {
+  /* ================= PRELOAD (NON-BLOCKING) ================= */
+  useEffect(() => {
+    // يمنع أي blocking
+    if (typeof window !== "undefined") {
+      requestIdleCallback(() => {
+        console.log("App ready");
+      });
+    }
+  }, []);
+
   return (
     <>
+      {/* ================= SEO BASE ================= */}
       <Head>
         <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index,follow,max-image-preview:large" />
+        <meta name="googlebot" content="index,follow,max-image-preview:large" />
+        <meta name="theme-color" content="#111827" />
+        <link rel="icon" href="/favicon.ico" />
 
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
+        {/* ================= PERFORMANCE HINTS ================= */}
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
 
-        <meta
-          name="robots"
-          content="index,follow,max-image-preview:large"
-        />
+        <link rel="preconnect" href="https://pagead2.googlesyndication.com" />
+        <link rel="dns-prefetch" href="https://pagead2.googlesyndication.com" />
 
-        <meta
-          name="googlebot"
-          content="index,follow,max-image-preview:large"
-        />
-
-        <meta
-          name="theme-color"
-          content="#111827"
-        />
-
-        <link
-          rel="icon"
-          href="/favicon.ico"
-        />
-
-        {/* PERFORMANCE */}
-        <link
-          rel="preconnect"
-          href="https://www.googletagmanager.com"
-          crossOrigin="anonymous"
-        />
-
-        <link
-          rel="preconnect"
-          href="https://connect.facebook.net"
-          crossOrigin="anonymous"
-        />
-
-        <link
-          rel="dns-prefetch"
-          href="https://www.googletagmanager.com"
-        />
-
-        <link
-          rel="dns-prefetch"
-          href="https://connect.facebook.net"
-        />
+        <link rel="preconnect" href="https://connect.facebook.net" />
+        <link rel="dns-prefetch" href="https://connect.facebook.net" />
       </Head>
 
-      {/* ================= GOOGLE TAG MANAGER ================= */}
-
+      {/* ================= GTM (DEFERRED) ================= */}
       <Script
         id="gtm-script"
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             (function(w,d,s,l,i){
               w[l]=w[l]||[];
-              w[l].push({
-                'gtm.start': new Date().getTime(),
-                event:'gtm.js'
-              });
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
               var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),
-              dl=l!='dataLayer'
-                ? '&l='+l
-                : '';
+              dl=l!='dataLayer'?'&l='+l:'';
               j.async=true;
-              j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+
-                i+dl;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
               f.parentNode.insertBefore(j,f);
-            })(
-              window,
-              document,
-              'script',
-              'dataLayer',
-              'GTM-KNQM8KBN'
-            );
+            })(window,document,'script','dataLayer','GTM-KNQM8KBN');
           `,
         }}
       />
 
-      {/* ================= GA4 ================= */}
-
+      {/* ================= GA4 (DELAYED) ================= */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-YS8L61XLPR"
         strategy="lazyOnload"
       />
 
-      <Script
-        id="ga4-script"
-        strategy="lazyOnload"
-      >
+      <Script id="ga4-script" strategy="lazyOnload">
         {`
-          window.dataLayer =
-            window.dataLayer || [];
-
-          function gtag(){
-            dataLayer.push(arguments);
-          }
-
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag(
-            'config',
-            'G-YS8L61XLPR',
-            {
-              anonymize_ip: true,
-              send_page_view: true
-            }
-          );
+          gtag('config', 'G-YS8L61XLPR', {
+            anonymize_ip: true,
+            send_page_view: false
+          });
+
+          window.addEventListener('load', () => {
+            gtag('event', 'page_view');
+          });
         `}
       </Script>
 
-      {/* ================= FACEBOOK PIXEL ================= */}
-
-      <Script
-        id="facebook-pixel"
-        strategy="lazyOnload"
-      >
+      {/* ================= FACEBOOK PIXEL (LAZY) ================= */}
+      <Script id="facebook-pixel" strategy="lazyOnload">
         {`
           !function(f,b,e,v,n,t,s){
             if(f.fbq)return;
             n=f.fbq=function(){
-              n.callMethod
-                ? n.callMethod.apply(
-                    n,
-                    arguments
-                  )
-                : n.queue.push(
-                    arguments
-                  );
+              n.callMethod ?
+              n.callMethod.apply(n,arguments) :
+              n.queue.push(arguments)
             };
-
-            if(!f._fbq)
-              f._fbq=n;
-
-            n.push=n;
-            n.loaded=!0;
-            n.version='2.0';
+            if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';
             n.queue=[];
-
             t=b.createElement(e);
             t.async=!0;
             t.src=v;
-
             s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)
+          }(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
 
-            s.parentNode.insertBefore(
-              t,
-              s
-            );
-          }(
-            window,
-            document,
-            'script',
-            'https://connect.facebook.net/en_US/fbevents.js'
-          );
-
-          fbq(
-            'init',
-            '1038760229314882'
-          );
-
-          fbq(
-            'track',
-            'PageView'
-          );
+          fbq('init', '1038760229314882');
+          fbq('track', 'PageView');
         `}
       </Script>
 
-      {/* ================= ADSENSE ================= */}
-
+      {/* ================= ADSENSE (LAZY ONLY) ================= */}
       <Script
-        id="adsense"
-        strategy="lazyOnload"
         async
+        strategy="lazyOnload"
         crossOrigin="anonymous"
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1294940976431468"
       />
 
-      {/* ================= PAGE ================= */}
-
+      {/* ================= LAYOUT ================= */}
       <Navbar />
 
-      <main>
+      <main style={{ minHeight: "80vh" }}>
         <Component {...pageProps} />
       </main>
 
@@ -211,4 +130,4 @@ export default function App({
       <SpeedInsights />
     </>
   );
-              }
+    }
