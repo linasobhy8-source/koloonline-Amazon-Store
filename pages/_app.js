f23af8ca14bb5a9e981f10e3e6654a752ef835b7
linasobhy8-source <linasobhy8@gmail.com>
2026-06-05 06:21:06 +0300

    Update _app.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -11,22 +11,35 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* ================= BASIC META ================= */}
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="index,follow,max-image-preview:large" />
+
+        <meta
+          name="robots"
+          content="index,follow,max-image-preview:large"
+        />
+
         <meta name="theme-color" content="#111827" />
       </Head>
 
-      {/* GTM (lazy فقط) */}
+      {/* ================= GTM OPTIMIZED ================= */}
       <Script
         id="gtm"
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
-          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KNQM8KBN');`,
+          __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KNQM8KBN');
+          `,
         }}
       />
 
@@ -38,7 +51,8 @@ export default function App({ Component, pageProps }) {
 
       <Footer />
 
+      {/* Vercel analytics (keep light) */}
       <SpeedInsights />
     </>
   );
-        }
+          }
