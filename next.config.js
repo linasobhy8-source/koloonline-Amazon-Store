d89f142b02c369261cd09aeb7d6f01ad8b60e5d5
linasobhy8-source <linasobhy8@gmail.com>
2026-06-10 00:48:25 +0300

    Update next.config.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- next.config.js
+++ next.config.js
@@ -5,21 +5,38 @@ const nextConfig = {
 
   compress: true,
   poweredByHeader: false,
+
   productionBrowserSourceMaps: false,
 
   images: {
     formats: ["image/avif", "image/webp"],
+
     remotePatterns: [
-      { protocol: "https", hostname: "m.media-amazon.com" },
-      { protocol: "https", hostname: "images-na.ssl-images-amazon.com" },
-      { protocol: "https", hostname: "**.googleusercontent.com" },
-      { protocol: "https", hostname: "**.firebaseapp.com" },
-      { protocol: "https", hostname: "via.placeholder.com" },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images-na.ssl-images-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.firebaseapp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
     ],
   },
 
   experimental: {
-    optimizePackageImports: [],
+    optimizePackageImports: ["firebase", "@vercel/speed-insights"],
   },
 };
 
