4eaab7725c8c8a485f6f2bb2a8acb1bd7f6a5bb8
linasobhy8-source <linasobhy8@gmail.com>
2026-06-09 14:38:56 +0300

    Update SEO and preconnect settings in _document.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_document.js
+++ pages/_document.js
@@ -1,42 +1,57 @@
-
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+
         {/* ================= BASIC SEO ================= */}
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="index,follow" />
+        <meta name="robots" content="index,follow,max-image-preview:large" />
         <meta name="googlebot" content="index,follow" />
-
-        {/* ================= THEME ================= */}
         <meta name="theme-color" content="#ff9900" />
 
-        {/* ================= ADSENSE (OPTIMIZED) ================= */}
+        {/* ================= DNS PREFETCH ================= */}
+        <link
+          rel="dns-prefetch"
+          href="//pagead2.googlesyndication.com"
+        />
+        <link
+          rel="dns-prefetch"
+          href="//www.google-analytics.com"
+        />
+        <link
+          rel="dns-prefetch"
+          href="//www.googletagmanager.com"
+        />
+
+        {/* ================= PRECONNECT ================= */}
+        <link
+          rel="preconnect"
+          href="https://pagead2.googlesyndication.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://www.google-analytics.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://www.googletagmanager.com"
+        />
+
+        {/* ================= ADSENSE ================= */}
         <meta
           name="google-adsense-account"
           content="ca-pub-1294940976431468"
         />
 
-        {/* preload ads script (faster load) */}
-        <link
-          rel="preconnect"
-          href="https://pagead2.googlesyndication.com"
-        />
-        <link rel="preconnect" href="https://www.google.com" />
-        <link rel="preconnect" href="https://www.google-analytics.com" />
-
-        {/* Adsense script (defer for speed) */}
         <script
           async
-          defer
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1294940976431468"
           crossOrigin="anonymous"
         />
 
-        {/* ================= STRUCTURED DATA ================= */}
+        {/* ================= WEBSITE SCHEMA ================= */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -54,6 +69,21 @@ export default function Document() {
             }),
           }}
         />
+
+        {/* ================= ORGANIZATION SCHEMA ================= */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "Koloonline",
+              url: "https://koloonline.online",
+              logo: "https://koloonline.online/logo.png",
+            }),
+          }}
+        />
+
       </Head>
 
       <body>
@@ -62,4 +92,4 @@ export default function Document() {
       </body>
     </Html>
   );
-    }
+            }
