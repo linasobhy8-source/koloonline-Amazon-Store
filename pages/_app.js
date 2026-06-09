101851c0c27ffce9d75a7248cd8e99183caefda6
linasobhy8-source <linasobhy8@gmail.com>
2026-06-10 00:17:58 +0300

    Update _app.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,36 @@
 import Head from "next/head";
 import Script from "next/script";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Component } from "react";
+
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 
 import "../styles/globals.css";
 
-/* ================= ERROR BOUNDARY (CRASH PROTECTION) ================= */
+/* ================= ERROR BOUNDARY ================= */
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+
+    this.state = {
+      hasError: false,
+    };
   }
 
-  static getDerivedStateFromError() {
-    return { hasError: true };
+  static getDerivedStateFromError(error) {
+    console.error("App Error:", error);
+
+    return {
+      hasError: true,
+    };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Component Crash:", error);
+    console.error(errorInfo);
   }
 
   render() {
@@ -26,6 +40,10 @@ class ErrorBoundary extends Component {
           style={{
             padding: 20,
             fontFamily: "sans-serif",
+            minHeight: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
           ⚠️ Something went wrong. Please refresh the page.
@@ -45,17 +63,34 @@ export default function App({ Component, pageProps }) {
         <Head>
           {/* ================= BASIC META ================= */}
           <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          />
 
           <meta
             name="robots"
             content="index,follow,max-image-preview:large"
           />
 
-          <meta name="theme-color" content="#111827" />
+          <meta
+            name="theme-color"
+            content="#111827"
+          />
+
+          <meta
+            name="format-detection"
+            content="telephone=no"
+          />
+
+          <meta
+            httpEquiv="X-UA-Compatible"
+            content="IE=edge"
+          />
         </Head>
 
-        {/* ================= GTM OPTIMIZED ================= */}
+        {/* ================= GOOGLE TAG MANAGER ================= */}
         <Script
           id="gtm"
           strategy="afterInteractive"
@@ -63,17 +98,37 @@ export default function App({ Component, pageProps }) {
             __html: `
               (function(w,d,s,l,i){
                 w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                w[l].push({
+                  'gtm.start': new Date().getTime(),
+                  event:'gtm.js'
+                });
+
                 var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),
                 dl=l!='dataLayer'?'&l='+l:'';
+
                 j.async=true;
                 j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+
                 f.parentNode.insertBefore(j,f);
+
               })(window,document,'script','dataLayer','GTM-KNQM8KBN');
             `,
           }}
         />
+
+        {/* ================= GTM NOSCRIPT ================= */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KNQM8KBN"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
 
         {/* ================= LAYOUT ================= */}
         <Navbar />
@@ -84,7 +139,10 @@ export default function App({ Component, pageProps }) {
 
         <Footer />
 
-        {/* ================= ANALYTICS ================= */}
+        {/* ================= VERCEL ANALYTICS ================= */}
+        <Analytics />
+
+        {/* ================= SPEED INSIGHTS ================= */}
         <SpeedInsights />
       </>
     </ErrorBoundary>
