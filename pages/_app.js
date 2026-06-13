bd6677581c8266411460f542c6c706ac9ed66061
linasobhy8-source <linasobhy8@gmail.com>
2026-06-13 04:58:55 +0300

    Enhance head section with additional meta tags
    Added meta tags for author, application name, and keywords.
    
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -55,14 +55,71 @@ export default function App({ Component, pageProps }) {
         {/* ================= HEAD ================= */}
         <Head>
           <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="robots" content="index,follow,max-image-preview:large" />
-          <meta name="theme-color" content="#111827" />
-          <meta name="format-detection" content="telephone=no" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          />
+
+          <meta
+            name="robots"
+            content="index,follow,max-image-preview:large"
+          />
+
+          <meta
+            name="theme-color"
+            content="#111827"
+          />
+
+          <meta
+            name="format-detection"
+            content="telephone=no"
+          />
+
+          <meta
+            httpEquiv="X-UA-Compatible"
+            content="IE=edge"
+          />
+
+          <meta
+            name="author"
+            content="Koloonline"
+          />
+
+          <meta
+            name="application-name"
+            content="Koloonline"
+          />
+
+          <meta
+            name="keywords"
+            content="
+            Amazon Deals,
+            Amazon Haul,
+            Amazon Reviews,
+            Product Reviews,
+            Smart Watches,
+            Tech Gadgets,
+            AliExpress Deals,
+            Online Shopping,
+            Best Products,
+            Koloonline
+          "
+          />
+
+          <link
+            rel="icon"
+            href="/favicon.ico"
+          />
+
+          <link
+            rel="canonical"
+            href="https://koloonline.online"
+          />
         </Head>
 
         {/* ================= GTM ================= */}
+
         <Script
           id="gtm"
           strategy="afterInteractive"
@@ -70,19 +127,83 @@ export default function App({ Component, pageProps }) {
             __html: `
               (function(w,d,s,l,i){
                 w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                w[l].push({
+                  'gtm.start': new Date().getTime(),
+                  event:'gtm.js'
+                });
+
                 var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),
-                dl=l!='dataLayer'?'&l='+l:'';
+                dl=l!='dataLayer'
+                ? '&l='+l
+                : '';
+
                 j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                j.src=
+                'https://www.googletagmanager.com/gtm.js?id='
+                + i + dl;
+
                 f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KNQM8KBN');
+              })(
+                window,
+                document,
+                'script',
+                'dataLayer',
+                'GTM-KNQM8KBN'
+              );
             `,
           }}
         />
 
+        {/* ================= GA4 ================= */}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YS8L61XLPR"
+          strategy="afterInteractive"
+        />
+
+        <Script
+          id="ga4"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer =
+              window.dataLayer || [];
+
+              function gtag(){
+                dataLayer.push(arguments);
+              }
+
+              gtag('js', new Date());
+
+              gtag(
+                'config',
+                'G-YS8L61XLPR',
+                {
+                  page_path:
+                  window.location.pathname
+                }
+              );
+            `,
+          }}
+        />
+
+        {/* ================= GTM NOSCRIPT ================= */}
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KNQM8KBN"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
+
         {/* ================= LAYOUT ================= */}
+
         <Navbar />
 
         <main>
@@ -92,8 +213,9 @@ export default function App({ Component, pageProps }) {
         <Footer />
 
         {/* ================= SPEED INSIGHTS ================= */}
+
         <SpeedInsights />
       </>
     </ErrorBoundary>
   );
-    }
+              }
