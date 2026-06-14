4cf0b7a3166514ba2d610181d2005ebfed4bd0dc
linasobhy8-source <linasobhy8@gmail.com>
2026-06-14 06:35:11 +0300

    Add haulPages data for trending deals and gadgets
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

---
+++ data/haulPages.js
@@ -0,0 +1,18 @@
+export const haulPages = [
+{
+slug: "trending-now",
+title: "Trending Amazon Haul Deals",
+description:
+"Today's best Amazon Haul deals.",
+content:
+"Discover trending products and discounts."
+},
+{
+slug: "viral-gadgets",
+title: "Viral Gadgets",
+description:
+"Most popular gadgets.",
+content:
+"Latest gadgets going viral."
+}
+];
