41adfa24af7f9b868091d740fa88c3d9e05617f6
linasobhy8-source <linasobhy8@gmail.com>
2026-06-15 00:33:18 +0300

    Enhance sitemap generation with XML escaping
    Added XML escaping function and improved URL building for blog sitemap.
    
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/sitemap-blog.xml.js
+++ pages/sitemap-blog.xml.js
@@ -3,30 +3,59 @@ import { db } from "../config/firebase";
 
 const baseUrl = "https://koloonline.online";
 
+/* ================= XML ESCAPE ================= */
+function escapeXml(str = "") {
+  return String(str)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+function buildUrl(loc) {
+  return `
+  <url>
+    <loc>${escapeXml(loc)}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.6</priority>
+  </url>`;
+}
+
+/* ================= HANDLER ================= */
 export default async function handler(req, res) {
-  const snap = await getDocs(collection(db, "blog"));
+  try {
+    const snap = await getDocs(collection(db, "blog"));
 
-  const urls = [];
+    const urls = [];
 
-  snap.forEach((doc) => {
-    const data = doc.data();
+    snap.forEach((doc) => {
+      const data = doc.data();
 
-    if (!data.slug) return;
+      const slug = data?.slug || doc.id;
+      if (!slug) return;
 
-    urls.push(`
-      <url>
-        <loc>${baseUrl}/blog/${data.slug}</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.6</priority>
-      </url>
-    `);
-  });
+      urls.push(buildUrl(`${baseUrl}/blog/${slug}`));
+    });
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.join("\n")}
 </urlset>`;
 
-  res.setHeader("Content-Type", "application/xml");
-  res.status(200).send(xml);
-}
+    res.setHeader("Content-Type", "application/xml");
+    res.setHeader(
+      "Cache-Control",
+      "public, s-maxage=3600, stale-while-revalidate=86400"
+    );
+
+    return res.status(200).send(xml);
+  } catch (error) {
+    console.error("Sitemap Blog Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+        }
