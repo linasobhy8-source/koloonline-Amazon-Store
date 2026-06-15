ff447f9e6d707a1ce834da5fd813ca94e7c7c7e3
linasobhy8-source <linasobhy8@gmail.com>
2026-06-15 03:51:41 +0300

    Update and rename pages/sitemap.xml.js to pages/api/sitemap.xml.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -1,26 +1,32 @@
-const baseUrl = "https://koloonline.online";
+export async function getServerSideProps({ res }) {
+  const baseUrl = "https://koloonline.online";
 
-export default function handler(req, res) {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
   <sitemap>
     <loc>${baseUrl}/sitemap-main.xml</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
   </sitemap>
 
   <sitemap>
     <loc>${baseUrl}/sitemap-products.xml</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
   </sitemap>
 
   <sitemap>
     <loc>${baseUrl}/sitemap-blog.xml</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
   </sitemap>
 
 </sitemapindex>`;
 
   res.setHeader("Content-Type", "application/xml");
-  res.status(200).send(xml);
+  res.write(xml);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default function Sitemap() {
+  return null;
 }
