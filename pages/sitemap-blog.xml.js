5795d15562b92cd8c775c77be32050750e6f7877
linasobhy8-source <linasobhy8@gmail.com>
2026-06-15 07:02:07 +0300

    Simplify Firebase setup and XML response
    Refactor Firebase initialization and response handling.
    
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/sitemap-blog.xml.js
+++ pages/sitemap-blog.xml.js
@@ -1,14 +1,5 @@
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-import { initializeApp, getApps } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-};
-
-const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 export async function getServerSideProps({ res }) {
   try {
@@ -37,14 +28,16 @@ ${urls}
 </urlset>`;
 
     res.setHeader("Content-Type", "application/xml");
-    res.end(xml);
+    res.write(xml);
+    res.end();
 
     return { props: {} };
   } catch (e) {
     console.error(e);
 
     res.setHeader("Content-Type", "application/xml");
-    res.end(`<?xml version="1.0"?><urlset></urlset>`);
+    res.write(`<?xml version="1.0"?><urlset></urlset>`);
+    res.end();
 
     return { props: {} };
   }
