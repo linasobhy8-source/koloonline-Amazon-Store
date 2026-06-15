77999775572e77d5213862ccdbc92ff734faa191
linasobhy8-source <linasobhy8@gmail.com>
2026-06-15 06:40:01 +0300

    Refactor sitemap-blog.xml.js for Firestore integration
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/sitemap-blog.xml.js
+++ pages/sitemap-blog.xml.js
@@ -1,5 +1,14 @@
-import { getDocs, collection } from "firebase/firestore";
-import { db } from "../config/firebase";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { initializeApp, getApps } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export async function getServerSideProps({ res }) {
   try {
@@ -7,43 +16,37 @@ export async function getServerSideProps({ res }) {
 
     const snap = await getDocs(collection(db, "blog"));
 
-    const urls = [];
+    let urls = "";
 
     snap.forEach((doc) => {
       const data = doc.data();
       const slug = data?.slug || doc.id;
 
-      urls.push(`
+      urls += `
         <url>
           <loc>${baseUrl}/blog/${slug}</loc>
           <changefreq>weekly</changefreq>
           <priority>0.6</priority>
         </url>
-      `);
+      `;
     });
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.join("\n")}
+${urls}
 </urlset>`;
 
     res.setHeader("Content-Type", "application/xml");
-    res.write(xml);
-    res.end();
+    res.end(xml);
 
-    return {
-      props: {},
-    };
-  } catch (error) {
-    console.error(error);
+    return { props: {} };
+  } catch (e) {
+    console.error(e);
 
     res.setHeader("Content-Type", "application/xml");
-    res.write(`<?xml version="1.0" encoding="UTF-8"?><urlset></urlset>`);
-    res.end();
+    res.end(`<?xml version="1.0"?><urlset></urlset>`);
 
-    return {
-      props: {},
-    };
+    return { props: {} };
   }
 }
 
