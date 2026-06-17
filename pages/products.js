74fcc5970b450a31cd9a1f92afa490c6dc67662a
linasobhy8-source <linasobhy8@gmail.com>
2026-06-17 21:28:54 +0300

    Update products.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/products.js
+++ pages/products.js
@@ -2,20 +2,29 @@ import useProducts from "../hooks/useProducts";
 import { safeText, safeImage } from "../lib/safe";
 
 export default function Products() {
-  const { products, loading } = useProducts();
+  const { products = [], loading } = useProducts();
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
       <h1>Products</h1>
 
-      {products?.map((p) => (
-        <div key={p.id}>
-          <img src={safeImage(p.image)} width={100} />
-          <p>{safeText(p.title)}</p>
-        </div>
-      ))}
+      {Array.isArray(products) &&
+        products.map((p) => (
+          <div key={String(p?.id || Math.random())}>
+            <img
+              src={safeImage(p?.image)}
+              alt={safeText(p?.title)}
+              width={100}
+              loading="lazy"
+            />
+
+            <p>{safeText(p?.title)}</p>
+          </div>
+        ))}
     </div>
   );
-}
+                }
