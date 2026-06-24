b00b8a6181391fdf4c37d1364311394c1c1d782a
linasobhy8-source <linasobhy8@gmail.com>
2026-06-24 13:35:22 +0300

    Update sitemap-top.xml.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/sitemap-top.xml.js
+++ pages/sitemap-top.xml.js
@@ -1,23 +1,31 @@
 import { topPages } from "../data/topPages";
 
 export async function getServerSideProps({ res }) {
-const baseUrl = "https://koloonline.online";
+  const baseUrl = "https://koloonline.online";
 
-const urls = topPages
-.map(
-(page) => " <url> <loc>${baseUrl}/top/${page.slug}</loc> <changefreq>weekly</changefreq> <priority>0.8</priority> </url>"
-)
-.join("");
+  const urls = topPages
+    .map(
+      (page) => `
+<url>
+  <loc>${baseUrl}/top/${page.slug}</loc>
+  <changefreq>weekly</changefreq>
+  <priority>0.8</priority>
+</url>`
+    )
+    .join("");
 
-const xml = "<?xml version="1.0" encoding="UTF-8"?> <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> ${urls} </urlset>";
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
 
-res.setHeader("Content-Type", "application/xml");
-res.write(xml);
-res.end();
+  res.setHeader("Content-Type", "application/xml");
+  res.write(xml);
+  res.end();
 
-return { props: {} };
+  return { props: {} };
 }
 
 export default function SitemapTop() {
-return null;
+  return null;
 }
