e8ecff67307abc7476ecb6e2c97def5b94f0f935
linasobhy8-source <linasobhy8@gmail.com>
2026-06-24 12:17:16 +0300

    Update sitemap.xml.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -1,32 +1,28 @@
 export async function getServerSideProps({ res }) {
-  const baseUrl = "https://koloonline.online";
+const baseUrl = "https://koloonline.online";
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
   <sitemap>
     <loc>${baseUrl}/sitemap-main.xml</loc>
-  </sitemap>
-
-  <sitemap>
+  </sitemap>  <sitemap>
     <loc>${baseUrl}/sitemap-products.xml</loc>
-  </sitemap>
-
-  <sitemap>
+  </sitemap>  <sitemap>
     <loc>${baseUrl}/sitemap-blog.xml</loc>
-  </sitemap>
+  </sitemap>  <sitemap>
+    <loc>${baseUrl}/sitemap-top.xml</loc>
+  </sitemap></sitemapindex>`;
 
-</sitemapindex>`;
+res.setHeader("Content-Type", "application/xml");
+res.write(xml);
+res.end();
 
-  res.setHeader("Content-Type", "application/xml");
-  res.write(xml);
-  res.end();
-
-  return {
-    props: {},
-  };
+return {
+props: {},
+};
 }
 
 export default function Sitemap() {
-  return null;
+return null;
 }
