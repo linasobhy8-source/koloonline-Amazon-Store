86930616a3600143c5c1548bc1ec87fa58fe15a9
linasobhy8-source <linasobhy8@gmail.com>
2026-07-02 17:44:29 +0300

    Update sitemap.xml.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -3,10 +3,16 @@ export async function getServerSideProps({ res }) {
     process.env.NEXT_PUBLIC_SITE_URL || "https://koloonline.online";
 
   try {
-    const apiRes = await fetch(`${baseUrl}/api/sitemap`);
-    const xml = await apiRes.text();
+    const response = await fetch(`${baseUrl}/api/sitemap`);
 
-    res.setHeader("Content-Type", "application/xml");
+    if (!response.ok) {
+      throw new Error(`API returned ${response.status}`);
+    }
+
+    const xml = await response.text();
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/xml; charset=UTF-8");
     res.setHeader(
       "Cache-Control",
       "public, s-maxage=3600, stale-while-revalidate=86400"
@@ -15,8 +21,11 @@ export async function getServerSideProps({ res }) {
     res.write(xml);
     res.end();
   } catch (err) {
+    console.error("Sitemap Error:", err);
+
     res.statusCode = 500;
-    res.end("Sitemap error");
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Sitemap Error");
   }
 
   return {
