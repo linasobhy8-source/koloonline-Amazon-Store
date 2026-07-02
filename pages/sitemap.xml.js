0b7eeca584cdcc021810bccc8092abe606ff7c0d
linasobhy8-source <linasobhy8@gmail.com>
2026-07-02 08:37:39 +0300

    Update sitemap.xml.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,12 +1,33 @@
 export default async function handler(req, res) {
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  try {
+    const baseUrl = "https://www.koloonline.online";
+
+    // نجيب البيانات من API الداخلي
+    const response = await fetch(`${baseUrl}/api/sitemap-data`);
+    const data = await response.json();
+
+    const urls = data.urls || [];
+
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (u) => `
   <url>
-    <loc>https://www.koloonline.online/</loc>
-    <priority>1.0</priority>
-  </url>
+    <loc>${u.loc}</loc>
+    <lastmod>${u.lastmod || ""}</lastmod>
+    <changefreq>${u.changefreq || "weekly"}</changefreq>
+    <priority>${u.priority || 0.8}</priority>
+  </url>`
+  )
+  .join("")}
 </urlset>`;
 
-  res.setHeader("Content-Type", "application/xml");
-  res.status(200).send(xml);
+    res.setHeader("Content-Type", "application/xml");
+    res.setHeader("Cache-Control", "public, s-maxage=3600");
+
+    return res.status(200).send(xml);
+  } catch (e) {
+    return res.status(500).send("sitemap error");
+  }
 }
