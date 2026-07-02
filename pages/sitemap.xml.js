1174a7321ad1d07ea60500590ecfcb6c585363a3
linasobhy8-source <linasobhy8@gmail.com>
2026-07-02 16:34:42 +0300

    Update sitemap.xml.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,31 +1,10 @@
-export default async function handler(req, res) {
+export async function getServerSideProps({ res }) {
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL || "https://koloonline.online";
+
   try {
-    const baseUrl =
-      process.env.NEXT_PUBLIC_SITE_URL || "https://koloonline.online";
-
     const apiRes = await fetch(`${baseUrl}/api/sitemap`);
-    const data = await apiRes.json();
-
-    const urls =
-      data?.urls ||
-      data?.data ||
-      data?.trending ||
-      [];
-
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls
-  .map(
-    (u) => `
-  <url>
-    <loc>${u.loc}</loc>
-    <lastmod>${u.lastmod || new Date().toISOString()}</lastmod>
-    <changefreq>${u.changefreq || "weekly"}</changefreq>
-    <priority>${u.priority || 0.7}</priority>
-  </url>`
-  )
-  .join("")}
-</urlset>`;
+    const xml = await apiRes.text();
 
     res.setHeader("Content-Type", "application/xml");
     res.setHeader(
@@ -33,9 +12,18 @@ ${urls
       "public, s-maxage=3600, stale-while-revalidate=86400"
     );
 
-    return res.status(200).send(xml);
-  } catch (e) {
-    console.error(e);
-    return res.status(500).send("Sitemap error");
+    res.write(xml);
+    res.end();
+  } catch (err) {
+    res.statusCode = 500;
+    res.end("Sitemap error");
   }
+
+  return {
+    props: {},
+  };
+}
+
+export default function Sitemap() {
+  return null;
 }
