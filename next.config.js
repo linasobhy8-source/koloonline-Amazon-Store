d9a3894a676d1e9f25ac593bd34ccbadd297ded3
linasobhy8-source <linasobhy8@gmail.com>
2026-07-04 02:09:28 +0300

    Update next.config.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- next.config.js
+++ next.config.js
@@ -93,4 +93,10 @@ const nextConfig = {
             key: "Cache-Control",
             value: "public, max-age=31536000, immutable",
           },
-       
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
